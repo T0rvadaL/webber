4b4b81f38b324ac2925e2b932f151581f22fe65d
Gutti Torvadal <gutti.torvadal@gmail.com>
2024-08-27 14:18:00 +0200

    Moved some print statements to event_hooks and switched to http1.1 as default
    Getting a lot of ConnectErrors and ReadErrors with http2, so I am trying http1

--- webber/_webber.py
+++ webber/_webber.py
@@ -6,6 +6,7 @@ import ua_generator
 
 from ._host_manager import HostManager
 from ._proxy import Proxy
+from ._request import Request
 
 
 class Webber:
@@ -22,7 +23,7 @@ class Webber:
             url: str,
             headers: httpx._types.HeaderTypes,
             event_hooks: typing.Mapping[str, list[httpx._client.EventHook]] | None = None,
-            http2: bool | None = True
+            http2: bool | None = False
     ) -> httpx.Response:
         host_name = httpx.URL(url).host
         host = self._hosts.setdefault(host_name, HostManager(self.proxies))
@@ -35,12 +36,22 @@ class Webber:
             httpx.ReadTimeout: 2,
             httpx.ProxyError: 4,
         }
+
+        if event_hooks is not None:
+            _event_hooks = {
+                "request": list(event_hooks.get("request", [])),
+                "response": list(event_hooks.get("response", [])),
+            }
+        else:
+            _event_hooks = {"request": [], "response": []}
+
+        event_hooks["request"].append(self._on_request)
+        event_hooks["response"].append(self._on_response)
+
         while True:
             try:
-                print(f"Requesting {url}...")
                 response = await host.request(url, headers, event_hooks, http2)
                 response.raise_for_status()
-                print(f"{url} succeeded.")
                 return response
 
             except httpx.HTTPStatusError as e:
@@ -74,4 +85,12 @@ class Webber:
         return ua_generator.generate(platform=("windows", "macos"),
                                      browser=("chrome", "edge", "firefox", "safari")
                                      ).headers.get()
+
+    @staticmethod
+    async def _on_request(request: Request):
+        print(f"Requesting {request.url}")
+
+    @staticmethod
+    async def _on_response(response: httpx.Response):
+        print(f"{response.url} succeeded.")
 
